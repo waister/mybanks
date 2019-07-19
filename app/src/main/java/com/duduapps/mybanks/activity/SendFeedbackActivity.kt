6bc040cc7508550670aa015904d84081577b9b2a
WaisterGrupo <waistergrupo@gmail.com>
2019-07-19 17:07:04 -0300

    Version 2 release - Adding rewarded video and feedback screen

---
+++ app/src/main/java/com/duduapps/mybanks/activity/SendFeedbackActivity.kt
@@ -0,0 +1,12 @@
+package com.duduapps.mybanks.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SendFeedbackActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_feedback)
+    }
+}
